8d5ab962e1cbab7791b72b2bb719242f892a2583
ds <ds@local>
2025-06-29 01:45:38 +0300

    restore ignore

--- install/create-docker-volumes.sh
+++ install/create-docker-volumes.sh
@@ -1,11 +1,12 @@
 echo "${_group}Creating volumes for persistent storage ..."
 set -x
 create_volume() {
+  local name=$1
   declare -a create_command=("$CONTAINER_ENGINE" volume create)
   if [ "$CONTAINER_ENGINE" = "podman" ]; then
-    create_command+=("$1")
+    create_command+=(--ignore "$name")
   else
-    create_command+=("--name=$1")
+    create_command+=("--name=$name")
   fi
 
   "${create_command[@]}"
