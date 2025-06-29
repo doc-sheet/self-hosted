9638a198603371ea907ed1a6659bbfa665855ac5
ds <ds@local>
2025-06-30 01:52:46 +0300

    test

--- install/bootstrap-snuba.sh
+++ install/bootstrap-snuba.sh
@@ -6,7 +6,7 @@ docker compose ps
 docker compose logs --names --timestamps || :
 docker logs sentry-self-hosted-clickhouse-1 || :
 if [[ -z "${SKIP_SNUBA_MIGRATIONS:-}" ]]; then
-  $dcr snuba-api bootstrap --force --log-level debug
+  $dcr snuba-api bootstrap --force
 else
   echo "Skipped DB migrations due to SKIP_SNUBA_MIGRATIONS=$SKIP_SNUBA_MIGRATIONS"
 fi
